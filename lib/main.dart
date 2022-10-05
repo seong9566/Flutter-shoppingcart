06abbe57c170e0a8c4bb7eb9098b90c62dcf6d5a
Goldfish808 <tjddns0853@gmail.com>
2022-10-05 09:45:06 +0900

    상태 ì 낯 ½드로 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 // 실행 클래스
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     // const만 붙이면 똑같은 객체가 2개가 절대 만들어지지 않는다
@@ -22,26 +21,55 @@ class MyApp extends StatelessWidget {
 // stateless는 정적인 파일이다.
 // 즉 StatelessWidget를 만들면 항상 똑같은 그림만 그린다.
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  //stateless는 무상태이지만, String update를 선언하면 상태가 생긴다.-> 무상태로 선언했는데 왜 상태를 만들어?
+  // 그러므로 final을 붙여준다. final을 상태가 없기 때문이다.
 
+  const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     //statless는 정적이기 때문에 동일한 객체를 만들지 않기 위해서 const를 붙인다.
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("클릭됌");
+        },
+      ),
       body: Column(
         children: [
-          Container(
-            color: Colors.yellow,
-            height: 200,
-            child: Text("낯"),
-          ),
+          DayComponent(),
           Container(
             color: Colors.blue,
             height: 200,
-            child: Text("밤"),
+            child: Text(
+              "밤",
+              style: TextStyle(fontSize: 50),
+            ),
           )
         ],
       ),
     ); //const는
+  }
+}
+
+class DayComponent extends StatefulWidget {
+  const DayComponent({Key? key}) : super(key: key);
+
+  @override
+  State<DayComponent> createState() => _DayComponentState();
+}
+
+class _DayComponentState extends State<DayComponent> {
+  String upData = "낯";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+      height: 200,
+      child: Text(
+        "${upData}",
+        style: TextStyle(fontSize: 50),
+      ),
+    );
   }
 }
