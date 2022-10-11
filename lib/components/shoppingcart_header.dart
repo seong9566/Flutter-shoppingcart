4a2327434d00a3af2a816c9bb7571fbc3ba585fe
Goldfish808 <tjddns0853@gmail.com>
2022-10-11 09:49:58 +0900

    header 틀만 잡아두기

--- lib/components/shoppingcart_header.dart
+++ lib/components/shoppingcart_header.dart
@@ -22,6 +22,19 @@ class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildHeaderPic(),
+        _buildHeaderSelector(),
+      ],
+    );
+  }
+
+  Widget _buildHeaderPic() {
+    return SizedBox();
+  }
+
+  Widget _buildHeaderSelector() {
     return SizedBox();
   }
 }
